fd3e3317d471ebdfccadeefa262d1beb9e9d0edb
Suraj Air <suraj.air@contentstack.com>
2023-10-03 17:22:43 +0530

    step - 11- with react-query

--- src/components/ProductsList.tsx
+++ src/components/ProductsList.tsx
@@ -1,5 +1,6 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { filter } from "lodash";
+import { useQuery } from "@tanstack/react-query";
 
 type Product = {
   brand: string;
@@ -24,76 +25,24 @@ type ProductsApiResponseData = {
 const API_URL = "https://dummyjson.com/products";
 
 /**
- * Global data store
- */
-const GLOBAL_DATA: {
-  [url: string]: {
-    state: "NOT_FETCHED" | "FETCHING" | "COMPLETE";
-    data?: ProductsApiResponseData;
-  };
-} = {};
-
-/**
- * Retry API call if it fails based on number of retry attempts
- * @param url
- * @param retryAttempt
- */
-const retryApiCall = async (
-  url: string,
-  retryAttempt = 2,
-): Promise<ProductsApiResponseData> => {
-  try {
-    const response = await fetch(url);
-    const data = (await response.json()) as ProductsApiResponseData;
-    return data as ProductsApiResponseData;
-  } catch (error) {
-    if (retryAttempt === 0) {
-      throw new Error((error as Error).message);
-    }
-    return retryApiCall(url, retryAttempt - 1);
-  }
-};
-
-/**
  * Custom hook to fetch products data
  * @returns {data, loading, error, refresh}
  */
-function useProducts(brand: string) {
-  const [data, setData] = useState<Product[] | undefined>();
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState<Error | undefined>();
-
-  useEffect(() => {
-    refresh();
-  }, []);
-
-  const refresh = () => {
-    if (GLOBAL_DATA[API_URL]?.state === "COMPLETE") {
-      setData(filter(GLOBAL_DATA[API_URL].data?.products, { brand }));
-      setLoading(false);
-      setError(undefined);
-      return;
-    }
-
-    if (GLOBAL_DATA[API_URL]?.state === "FETCHING") {
-      setTimeout(() => refresh(), 1000);
-      return;
-    }
-    GLOBAL_DATA[API_URL] = { state: "FETCHING", data: undefined };
-    setLoading(true);
-    setError(undefined);
-    retryApiCall(API_URL, 2)
-      .then((data: ProductsApiResponseData) => {
-        GLOBAL_DATA[API_URL] = { state: "COMPLETE", data };
-        setData(filter(data.products, { brand }));
-        setLoading(false);
-      })
-      .catch((error: Error) => {
-        GLOBAL_DATA[API_URL] = { state: "NOT_FETCHED", data: undefined };
-        setError(error);
-      })
-      .finally(() => setLoading(false));
-  };
+export function useProducts(brand: string) {
+  const {
+    data,
+    isLoading: loading,
+    error,
+    refetch: refresh,
+  } = useQuery({
+    queryKey: ["products"],
+    queryFn: async () => {
+      const response = await fetch(API_URL);
+      const data = (await response.json()) as ProductsApiResponseData;
+      return filter(data.products, { brand });
+    },
+    enabled: !!brand,
+  });
 
   return { data, loading, error, refresh };
 }
@@ -101,7 +50,7 @@ function useProducts(brand: string) {
 export const ProductsList = ({ brand }: { brand: string }) => {
   const { data, loading, error, refresh } = useProducts(brand);
 
-  if (loading)
+  if (loading || !data)
     return (
       <div className={"border border-gray-200 p-3 rounded-md"}>
         <div>Loading...</div>
@@ -112,7 +61,10 @@ export const ProductsList = ({ brand }: { brand: string }) => {
       <div className={"border border-gray-200 p-3 rounded-md"}>
         <div className="text-red-400">Error: Something went wrong.</div>
         <p>
-          <button className="p-3 py-1 rounded-md bg-gray-200" onClick={refresh}>
+          <button
+            className="p-3 py-1 rounded-md bg-gray-200"
+            onClick={() => refresh()}
+          >
             Retry
           </button>
         </p>
