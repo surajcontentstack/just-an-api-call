6ec02f27d315ba1735899494026b917cccfae0aa
Suraj Air <suraj.air@contentstack.com>
2023-10-03 10:27:33 +0530

    feat: step 1

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="h-screen w-screen flex flex-col space-y-2 justify-center items-center">
+      <h2 className="text-5xl font-bold text-blue-500">Just An Api Call</h2>
+      <p>Handling Async Functions In ReactJs</p>
+    </div>
+  );
+}
+
+export default App;
