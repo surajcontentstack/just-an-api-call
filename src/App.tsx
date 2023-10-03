5ef6466a9c731693e03afe746404448ecc75af6a
Suraj Air <suraj.air@contentstack.com>
2023-10-03 15:33:04 +0530

    step 10- react query

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,21 @@
 import { ProductsList } from "./components/ProductsList.tsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+// Create a client
+const queryClient = new QueryClient();
 
 function App() {
   return (
-    <div className="grid grid-cols-2 lg:grid-cols-4">
-      <div className="p-4">
-        <ProductsList brand={"Apple"} />
+    <QueryClientProvider client={queryClient}>
+      <div className="grid grid-cols-2 lg:grid-cols-4">
+        <div className="p-4">
+          <ProductsList brand={"Apple"} />
+        </div>
+        <div className="p-4">
+          <ProductsList brand={"Samsung"} />
+        </div>
       </div>
-      <div className="p-4">
-        <ProductsList brand={"Samsung"} />
-      </div>
-    </div>
+    </QueryClientProvider>
   );
 }
 
